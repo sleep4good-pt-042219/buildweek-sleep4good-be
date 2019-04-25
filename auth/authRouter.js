b82070073b108d9bf139b46f7cbdf4d32c9fd23a
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-25 11:53:14 -0400

    add auth user register route, hash password

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
+const Users = require('./../data/helpers/usersDbHelper');
 
 router.post('/register', (req, res) => {
-   res.status(200).json('This is a register route')
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    Users.insert(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
 });
 
 module.exports = router;
