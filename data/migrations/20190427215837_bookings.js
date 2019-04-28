92494146f01c82d58735f53f3c9fbac4dc131cc9
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-28 11:21:00 -0400

    add bookings migration table file

---
+++ data/migrations/20190427215837_bookings.js
@@ -0,0 +1,39 @@
+exports.up = function(knex) {
+    
+    return knex.schema
+    .createTable('bookings', function(bookings) {
+        bookings.increments();
+        bookings
+            .integer('location_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('locations')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        bookings
+            .integer('hotel_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('hotels')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        bookings
+            .integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        bookings
+            .integer('total', 55)
+            .notNullable();
+        
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('bookings')
+};
