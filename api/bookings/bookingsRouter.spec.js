64ec36a4b4e07316881f9e1d1e3dd43149ebe69a
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-02 22:03:44 -0400

    testing post method to bookings

--- api/bookings/bookingsRouter.spec.js
+++ api/bookings/bookingsRouter.spec.js
@@ -27,56 +27,58 @@ describe('the server', () => {
                 password: 'adsdadsd',
                 role_id: 3
             }
-            // console.log(user)
+
             const token = generateToken(user);
-            // console.log(token)
-            const res = await request(server).get("/api/bookings").set('Accept', 'application/json')
-            .set('Authorization', token);
-            // console.log(res.body)
+            const res = await request(server)
+                .get("/api/bookings")
+                .set('Accept', 'application/json')
+                .set('Authorization', token);
+
             expect(res.status).toBe(200);
 
-            // res.expect('Content-Type', /json/)
-            // res.expect(200, done);
         });
 
         it('responds with 401', async function() {
 
-            const res = await request(server).get("/api/bookings").set('Accept', 'application/json')
-            .set('Authorization', null);
+            const res = await request(server)
+                .get("/api/bookings")
+                .set('Accept', 'application/json')
+                .set('Authorization', null);
+
             expect(res.status).toBe(401);
 
         });
+
+        it('responds with 201', async function() {
+            const user = {
+                username: 'Jojojoj',
+                password: 'adsdadsd',
+                role_id: 3
+            }
+            const booking = { 
+                location_id: 3,
+                hotel_id: 2,
+                user_id: 1,
+                total: 33,
+                booking_start_date: '2014-12-30 23:18:54',
+                booking_end_date: '2006-12-30 00:38:54'
+            }
+            const token = generateToken(user);
+
+            const res = await request(server)
+                .post("/api/bookings")
+                .send(booking)
+                .set('Accept', 'application/json')
+                .set('Authorization', token);
+
+            expect(res.status).toBe(201);
+            // console.log(res.body.booking)
+            expect(res.body.booking.location_id).toBe(3)
+            expect(res.body.booking.total).toBe(33)
+            expect(res.body.booking.booking_end_date).toBe('2006-12-30 00:38:54')
+
+
+        });
     });
-    // it('should respond with all bookings in the db', async () => {
-    //     await db('bookings').insert([
-    //         { 
-    //             location_id: 3,
-    //             hotel_id: 2,
-    //             user_id: 1,
-    //             total: 33,
-    //             booking_start_date: '2014-12-30 23:18:54',
-    //             booking_end_date: '2006-12-30 00:38:54'
-    //         },
-    //         { 
-    //             location_id: 3,
-    //             hotel_id: 2,
-    //             user_id: 1,
-    //             total: 53,
-    //             booking_start_date: '2014-12-30 23:18:54',
-    //             booking_end_date: '2006-12-30 00:38:54'
-    //         }
-    //     ])
-
-    //     const res = await request(server).get('/api/bookings');
-    //     expect(res.body.length).toBe(2)
-    //     expect(res.status).toBe(200);
-    //     expect(res.body[0].total).toBe(33)
-    //     expect(res.body[1].total).toBe(33)
-    //     expect(res.body[0].id).toBe(1)
-    //     expect(res.body[1].id).toBe(2)
-    //     expect(res.type).toBe('application/json');
-
-    // })
-    // })
 
 })
